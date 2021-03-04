c1f0e4221be26d9cfe49df898cc2fb805de46080
arms <arms_1991@hotmail.com>
2021-03-04 18:01:05 -0300

    add compras no projeto

--- aula08/spring02/src/main/java/br/arms/spring02/repository/UsuarioRepo.java
+++ aula08/spring02/src/main/java/br/arms/spring02/repository/UsuarioRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import br.arms.spring02.model.Usuario;
 
 public interface UsuarioRepo extends CrudRepository<Usuario,Integer>{
-
+    public Usuario findByEmailAndSenha(String email, String senha);
 }
 
